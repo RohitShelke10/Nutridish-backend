ce3e2d4122c48983409eefee4553f5e0162918bf
Rohit Shelke <rohitshelke11@gmail.com>
2022-09-21 15:22:04 +0530

    feat: added qr verification

--- src/routes/bookingRouter.ts
+++ src/routes/bookingRouter.ts
@@ -1,10 +1,12 @@
 import express from "express";
 const router = express.Router();
-import { book, createOrder } from "../controllers/bookingController";
+import { book, createOrder, deliver } from "../controllers/bookingController";
 import { requireAuth } from "../middleware/authMiddleware";
 
 router.post("/", requireAuth, book);
 
 router.post("/paymentlink", requireAuth, createOrder);
+
+router.get("/deliver/:bookingId", deliver);
 
 export default router;
