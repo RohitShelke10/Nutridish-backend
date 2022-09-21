add52c33c53335301d44e3ad494e9a64a0238f19
Rohit Shelke <rohitshelke11@gmail.com>
2022-09-21 14:34:49 +0530

    feat: daily menu API

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,6 +1,8 @@
 import { Response } from "express";
 import { IRequest } from "../types/types";
 import User from "../models/users";
+import { auth as authentication } from "../utils/spreadsheetsUtil";
+require("dotenv").config();
 
 export const editProfile = async (req: IRequest, res: Response) => {
   const { name, contact, buildingId, floorId, departmentId, room, isStaff } =
@@ -42,5 +44,28 @@ export const editProfile = async (req: IRequest, res: Response) => {
     res
       .status(400)
       .json({ success: false, message: "Incorrect request params" });
+  }
+};
+
+export const getMenu = async (req: IRequest, res: Response) => {
+  try {
+    const credentials = await authentication();
+    const auth = credentials.auth;
+    const googleSheetsInstance = credentials.googleSheetsInstance;
+    const spreadsheetId = process.env.SheetID;
+    const day = new Date().getDay() + 1;
+    const data = await googleSheetsInstance.spreadsheets.values.get({
+      auth,
+      spreadsheetId,
+      range: `Daily Menu!A${day}:B${day}`,
+    });
+    res.status(200).json({
+      data: {
+        day: data.data.values[0][0],
+        menu: data.data.values[0][1].split(","),
+      },
+    });
+  } catch (err) {
+    res.status(400).json(err);
   }
 };
