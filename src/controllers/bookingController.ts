9773608bd0bd98abb9b0b379df94e2d6b438beec
Rohit Shelke <rohitshelke11@gmail.com>
2022-09-21 17:09:47 +0530

    feat: added cod option

--- src/controllers/bookingController.ts
+++ src/controllers/bookingController.ts
@@ -67,95 +67,125 @@ export const book = async (req: IRequest, res: Response) => {
     paymentId
   ) {
     try {
-      const payment = await razorpay.paymentLink.fetch(paymentId);
-      if (payment.payments.length !== 0) {
-        const receipt = await Payment.create({
-          user: user!._id,
-          payment_id: payment.payments[0].payment_id,
-          reference_id: payment.reference_id,
-          status: payment.payments[0].status,
-          amount_paid: payment.amount_paid / 100,
-        });
-        if (payment.payments[0].status === "captured") {
-          const booking = await Booking.create({
+      if (paymentMode === "UPI") {
+        const payment = await razorpay.paymentLink.fetch(paymentId);
+        if (payment.payments.length !== 0) {
+          const receipt = await Payment.create({
             user: user!._id,
-            building: building,
-            department: department,
-            floor: floor,
-            room: room,
-            date: date,
-            paymentMode: paymentMode,
-            quantity: quantity,
-            paymentId: payment.payments[0].payment_id,
+            payment_id: payment.payments[0].payment_id,
+            reference_id: payment.reference_id,
+            status: payment.payments[0].status,
+            amount_paid: payment.amount_paid / 100,
           });
-          const result = await cloud.uploader.upload(
-            await QRCode.toDataURL(
-              `${process.env.SERVER_URL}/book/deliver/${booking._id}`
-            ),
-            {
-              overwrite: true,
-              folder: "qrcodes",
-              public_id: `${booking._id}-qr.png`,
-            }
-          );
-          await Booking.findByIdAndUpdate(booking._id, {
-            $set: { qr: result.secure_url },
-          });
-          // *Whatsapp messaging*
-          // if (user!.contact) {
-          //   await axios.post(
-          //     process.env.WA_URL!.toString(),
-          //     {
-          //       messaging_product: "whatsapp",
-          //       to: user!.contact,
-          //       type: "template",
-          //       template: {
-          //         name: "order_confirm",
-          //         language: {
-          //           code: "en_GB",
-          //         },
-          //         components: [
-          //           {
-          //             type: "header",
-          //             parameters: [
-          //               {
-          //                 type: "image",
-          //                 image: {
-          //                   link: "https://res.cloudinary.com/dpp7elupy/image/upload/v1662890116/nutridish/Photo_from_%E0%A4%B5%E0%A5%87%E0%A4%A6_pnbwqa.jpg",
-          //                 },
-          //               },
-          //             ],
-          //           },
-          //         ],
-          //       },
-          //     },
-          //     {
-          //       headers: {
-          //         "Content-Type": "application/json",
-          //         Authorization: `Bearer ${process.env.WA_TOKEN}`,
-          //       },
-          //     }
-          //   );
-          // }
-          res.status(200).json({
-            sucess: true,
-            message: "Success",
-            data: receipt,
-            qr: result.secure_url,
-          });
+          if (payment.payments[0].status === "captured") {
+            const booking = await Booking.create({
+              user: user!._id,
+              building: building,
+              department: department,
+              floor: floor,
+              room: room,
+              date: date,
+              paymentMode: paymentMode,
+              quantity: quantity,
+              paymentId: payment.payments[0].payment_id,
+            });
+            const result = await cloud.uploader.upload(
+              await QRCode.toDataURL(
+                `${process.env.SERVER_URL}/book/deliver/${booking._id}`
+              ),
+              {
+                overwrite: true,
+                folder: "qrcodes",
+                public_id: `${booking._id}-qr.png`,
+              }
+            );
+            await Booking.findByIdAndUpdate(booking._id, {
+              $set: { qr: result.secure_url },
+            });
+            // *Whatsapp messaging*
+            // if (user!.contact) {
+            //   await axios.post(
+            //     process.env.WA_URL!.toString(),
+            //     {
+            //       messaging_product: "whatsapp",
+            //       to: user!.contact,
+            //       type: "template",
+            //       template: {
+            //         name: "order_confirm",
+            //         language: {
+            //           code: "en_GB",
+            //         },
+            //         components: [
+            //           {
+            //             type: "header",
+            //             parameters: [
+            //               {
+            //                 type: "image",
+            //                 image: {
+            //                   link: "https://res.cloudinary.com/dpp7elupy/image/upload/v1662890116/nutridish/Photo_from_%E0%A4%B5%E0%A5%87%E0%A4%A6_pnbwqa.jpg",
+            //                 },
+            //               },
+            //             ],
+            //           },
+            //         ],
+            //       },
+            //     },
+            //     {
+            //       headers: {
+            //         "Content-Type": "application/json",
+            //         Authorization: `Bearer ${process.env.WA_TOKEN}`,
+            //       },
+            //     }
+            //   );
+            // }
+            res.status(200).json({
+              sucess: true,
+              message: "Success",
+              data: { booking: booking, qr: result.secure_url },
+            });
+          } else {
+            res.status(400).json({
+              success: false,
+              failed: true,
+              message: "Payment failed, please retry",
+              data: receipt,
+            });
+          }
         } else {
           res.status(400).json({
             success: false,
-            failed: true,
-            message: "Payment failed, please retry",
-            data: receipt,
+            failed: false,
+            message: "Payment not made yet",
           });
         }
-      } else {
-        res.status(400).json({
-          success: false,
-          failed: false,
-          message: "Payment not made yet",
+      } else if (paymentMode === "Pay On Delivery") {
+        const booking = await Booking.create({
+          user: user!._id,
+          building: building,
+          department: department,
+          floor: floor,
+          room: room,
+          date: date,
+          paymentMode: paymentMode,
+          quantity: quantity,
+        });
+        const result = await cloud.uploader.upload(
+          await QRCode.toDataURL(
+            `${process.env.SERVER_URL}/book/deliver/${booking._id}`
+          ),
+          {
+            overwrite: true,
+            folder: "qrcodes",
+            public_id: `${booking._id}-qr.png`,
+          }
+        );
+        await Booking.findByIdAndUpdate(booking._id, {
+          $set: { booking: booking, qr: result.secure_url },
+        });
+        res.status(200).json({
+          sucess: true,
+          message: "Success",
+          data: { qr: result.secure_url },
         });
       }
     } catch (err) {
@@ -177,9 +207,7 @@ export const deliver = async (req: IRequest, res: Response) => {
         await Booking.findByIdAndUpdate(bookingId, {
           $set: {
             isDelivered: true,
-            deliveredOn: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${
-              date.getSeconds()
-            }`,
+            deliveredOn: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`,
           },
           $unset: { qr: "" },
         });
