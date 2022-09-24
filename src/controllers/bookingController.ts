10a22a0e6d5bf6ccc93a272532311ceda8186638
Rohit Shelke <rohitshelke11@gmail.com>
2022-09-24 19:36:01 +0530

    fix: sunday orders

--- src/controllers/bookingController.ts
+++ src/controllers/bookingController.ts
@@ -16,48 +16,56 @@ export const createOrder = async (req: IRequest, res: Response) => {
 
   try {
     if (amount && date) {
-      let price;
-      user?.isStaff
-        ? (price = parseInt(process.env.STAFF_PRICE!))
-        : (price = parseInt(process.env.STUDENT_PRICE!));
-      const result = await razorpay.paymentLink.create({
-        upi_link: true,
-        amount: amount * price * 100,
-        currency: "INR",
-        description: "Nutri-Dish",
-        reference_id: otpGenerator.generate(10, {
-          digits: true,
-          specialChars: false,
-        }),
-        expire_by: Date.now() + 300000,
-        customer: {
-          name: req.user!.name,
-          email: req.user!.email,
-          contact: req.user!.contact,
-        },
-      });
-      await Booking.create({
-        user: user!._id,
-        date: date,
-        paymentMode: "UPI",
-        quantity: amount,
-        paymentId: result.id,
-        paid: false,
-        building: user?.building,
-        department: user?.department,
-        floor: user?.floor,
-        room: user?.room,
-        price: amount * price,
-      });
+      const day = date.split(" ")[0];
+      if (day !== "Sun") {
+        let price;
+        user?.isStaff
+          ? (price = parseInt(process.env.STAFF_PRICE!))
+          : (price = parseInt(process.env.STUDENT_PRICE!));
+        const result = await razorpay.paymentLink.create({
+          upi_link: true,
+          amount: amount * price * 100,
+          currency: "INR",
+          description: "Nutri-Dish",
+          reference_id: otpGenerator.generate(10, {
+            digits: true,
+            specialChars: false,
+          }),
+          expire_by: Date.now() + 300000,
+          customer: {
+            name: req.user!.name,
+            email: req.user!.email,
+            contact: req.user!.contact,
+          },
+        });
+        await Booking.create({
+          user: user!._id,
+          date: date,
+          paymentMode: "UPI",
+          quantity: amount,
+          paymentId: result.id,
+          paid: false,
+          building: user?.building,
+          department: user?.department,
+          floor: user?.floor,
+          room: user?.room,
+          price: amount * price,
+        });
 
-      res.status(200).json({
-        success: true,
-        data: {
-          payment_url: result.short_url,
-          payment_id: result.id,
-          reference_id: result.reference_id,
-        },
-      });
+        res.status(200).json({
+          success: true,
+          data: {
+            payment_url: result.short_url,
+            payment_id: result.id,
+            reference_id: result.reference_id,
+          },
+        });
+      } else {
+        res.status(400).json({
+          success: false,
+          message: "Orders can't be placed for Sunday",
+        });
+      }
     } else {
       res.status(400).json({
         success: false,
@@ -126,46 +134,57 @@ export const book = async (req: IRequest, res: Response) => {
   const { date, quantity } = req.body;
   const user = req.user;
   if (date && quantity) {
-    try {
+    const day = date.split(" ")[0];
+    if (day !== "Sun") {
       let price;
       user?.isStaff
         ? (price = parseInt(process.env.STAFF_PRICE!))
         : (price = parseInt(process.env.STUDENT_PRICE!));
-      const booking = await Booking.create({
-        user: user!._id,
-        date: date,
-        paymentMode: "Pay On Delivery",
-        quantity: quantity,
-        building: user?.building,
-        department: user?.department,
-        floor: user?.floor,
-        room: user?.room,
-        price: quantity * price,
-      });
+      try {
+        const booking = await Booking.create({
+          user: user!._id,
+          date: date,
+          paymentMode: "Pay On Delivery",
+          quantity: quantity,
+          building: user?.building,
+          department: user?.department,
+          floor: user?.floor,
+          room: user?.room,
+          price: quantity * price,
+        });
 
-      const result = await cloud.uploader.upload(
-        await QRCode.toDataURL(
-          `${process.env.SERVER_URL}/book/deliver/${booking._id}`
-        ),
-        {
-          overwrite: true,
-          folder: "qrcodes",
-          public_id: `${booking._id}-qr.png`,
-        }
-      );
-      await Booking.findByIdAndUpdate(booking._id, {
-        $set: { qr: result.secure_url },
+        const result = await cloud.uploader.upload(
+          await QRCode.toDataURL(
+            `${process.env.SERVER_URL}/book/deliver/${booking._id}`
+          ),
+          {
+            overwrite: true,
+            folder: "qrcodes",
+            public_id: `${booking._id}-qr.png`,
+          }
+        );
+        await Booking.findByIdAndUpdate(booking._id, {
+          $set: { qr: result.secure_url },
+        });
+        res.status(200).json({
+          success: true,
+          message: "Success",
+          data: { booking: booking, qr: result.secure_url },
+        });
+      } catch (err) {
+        res.status(400).json(err);
+      }
+    } else {
+      res.status(400).json({
+        success: false,
+        message: "Orders can't be placed for Sunday",
       });
-      res.status(200).json({
-        success: true,
-        message: "Success",
-        data: { booking: booking, qr: result.secure_url },
-      });
-    } catch (err) {
-      res.status(400).json(err);
     }
   } else {
-    res.status(400).json({ message: "Invalid data" });
+    res.status(400).json({
+      success: false,
+      message: "Invalid params",
+    });
   }
 };
 
